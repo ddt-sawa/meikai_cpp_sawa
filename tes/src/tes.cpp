0dba32d3d53943ad044c4b574e8f75a841d768a2
ddt-sawa <ddt.sawa@gmail.com>
2018-06-14 20:52:24 +0900

    add 新規ファイル

---
+++ tes/src/tes.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : tes.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin>>x >>y;
+	double ave = (x + y) / 2;
+			cout<< ave;
+}
