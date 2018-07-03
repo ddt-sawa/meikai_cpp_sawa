ccbe329db17f9dce4d12cd6e7d674ed2b6682e74
ddt-sawa <ddt.sawa@gmail.com>
2018-07-03 20:25:37 +0900

    mod text, 2-5 変数名・コメント・ブロック文修正

--- 2-5/2-5.cpp
+++ 2-5/2-5.cpp
@@ -16,33 +16,40 @@ using namespace std;
 
 int main()
 {
-	 //int型変数xの宣言
-	 int x;
+	 //3で割り切れるか調べたい整数を宣言
+	 int firstNumber;
 
-	 //xの入力を促す
+	 //整数値の入力を促す
 	 cout << "整数値 : ";
 
-	 //入力されたxの値を読み込む
-	 cin >> x;
+	 //整数値の入力
+	 cin >> firstNumber;
 
-	 //xが正の場合
-	 if (x > 0)
+	 //整数が正の場合
+	 if (firstNumber > 0){
 
-		 //xが3で割り切れる場合
-		 if (x % 3 == 0)
+		 //3で割り切れる場合
+		 if (firstNumber % 3 == 0){
 			 cout << "その値は3で割り切れます。\n";
+		 }
 
-	     //xが3で割ると1余る場合
-		 else if (x % 3 == 1)
+	     //3で割ると1余る場合
+		 else if (firstNumber % 3 == 1){
 			 cout << "その値を3で割った剰余は1です。\n";
+		 }
 
-	     //xが3で割ると2余る場合
-		 else
+	     //3で割ると2余る場合
+		 else {
 			 cout << "その値を3で割った剰余は2です。\n";
+		 }
+	 }
 
-	 //xが負及び0の場合
-	 else
+	 //整数が負及び0の場合
+	 else{
 		 cout << "正でない値が入力されました。\n";
+     }
+
 }
+
 
 
