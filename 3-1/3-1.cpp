79b77f47663cd704849b0d81ee89ff77054edd0c
ddt-sawa <ddt.sawa@gmail.com>
2018-07-03 21:25:23 +0900

    mod text, 3-1 変数名・コメント・ブロック文修正

--- 3-1/3-1.cpp
+++ 3-1/3-1.cpp
@@ -8,7 +8,7 @@
 /*演習3-1 読み込んだ整数値の符号を判定するList2-5(p44)を、好きなだけ何度でも
  * 入力・表示を繰り返せるようにしたプログラムを作成せよ。*/
 
-//string型を使うため、<string>ヘッダをインクルード
+//stringクラスを使うため、<string>ヘッダをインクルード
 #include<string>
 #include<iostream>
 
@@ -16,35 +16,45 @@ using namespace std;
 
 int main()
 {
-    //繰り返しを訪ねる際に用いる、siring型変数retryの宣言
-	string retry;
+    //再試行を行うかどうかを判定する文字列の宣言
+	string userRetry;
 
+    //入力者が望む回数、符号判定を繰り返す
     do {
-	// int型変数xの宣言
-	int x;
 
-	//整数の入力を促す
-	cout << "整数値 : ";
+    	// 符号を判定したい整数の宣言
+    	int firstNumber;
 
-	//入力された整数を読み込む
-	cin >> x;
+    	//整数の入力
+    	cout << "整数値 : ";  cin >> firstNumber;
 
-	//xが正の場合
-	if (x > 0)
-		cout << "その値は正です\n";
+    	//整数が正の場合
+    	if (firstNumber > 0){
 
-	//xが負の場合
-	else if (x < 0)
-		cout << "その値は負です\n";
+    		//値が正であることを表示
+    		cout << "その値は正です\n";
+    	}
 
-	//xが0の場合
-	else
-		cout << "その値は0です。\n";
+    	//整数が負の場合
+    	else if (firstNumber < 0){
 
-	//もう一度入力を行うかを訪ねる
-	cout << "もう一度？ Y・・・Yes / N・・・No";
+    		//値が負であることを表示
+    		cout << "その値は負です\n";
+    	}
 
-    //Y,yを検出した場合もう一回
-	cin >> retry;
-    }while(retry == "Y" || retry == "y");
+    	//整数が0の場合
+    	else{
+
+    		//値が0であることを表示
+    		cout << "その値は0です。\n";
+    	}
+
+    	//再試行するかを訪ねる
+    	cout << "もう一度？ Y・・・Yes / N・・・No";
+
+    	//YesorNoの入力
+    	cin >> userRetry;
+
+	//'Y','y'が入力された場合再試行
+    }while(userRetry == "Y" || userRetry == "y");
 }
