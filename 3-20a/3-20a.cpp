55377d4cc19756c703db9ea55c8c0333356df484
ddt-sawa <ddt.sawa@gmail.com>
2018-07-05 20:26:19 +0900

    mod text, 3-20a 変数名・コメント・ブロック文修正

--- 3-20a/3-20a.cpp
+++ 3-20a/3-20a.cpp
@@ -17,16 +17,25 @@ using namespace std;
 
 int main()
 {
-	//直角三角形の段数を入力させ、読み込む
-    int n;
-    cout << "段数 : ";
-	cin >> n;
+	//直角三角形の段数
+    int stepNumber;
 
-	    //n段の直角三角形を表示
-	for (int i = 1; i <= n; i++){
-	    //左上側に直角を持っていくため、段数が変わるごとにアスタリスクを減らす
-		for (int j = 0; j <= n - i; j++)
+    //段数入力を促す
+    cout << "段数 : ";
+
+    //段数入力
+	cin >> stepNumber;
+
+	    //n段の直角三角形を表示するループ文
+	for (int i = 1; i <= stepNumber; i++){
+
+	    //左上側に直角を持っていくため、段数が変わるごとに記号を減らすループ文
+		for (int j = 0; j <= stepNumber - i; j++) {
+
+			//記号を表示
 			cout << "*";
+		}
+		//段数を変えるため改行
 	cout << '\n';
 	}
 }
