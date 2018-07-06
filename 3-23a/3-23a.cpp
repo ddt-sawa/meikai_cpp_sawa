6b888f088218cab061db6196a2069b0b1b7126ac
ddt-sawa <ddt.sawa@gmail.com>
2018-07-06 15:01:16 +0900

    mod text, 3-23a 変数名・コメント・ブロック文・平均値の計算ミス修正

--- 3-23a/3-23a.cpp
+++ 3-23a/3-23a.cpp
@@ -7,7 +7,7 @@
 
 /*演習3-23 合計だけでなく平均も求めるように、List3-15およびList3-16を
  * 書き換えたプログラムをそれぞれ作成せよ。
- * a.List3-16の書き換え
+ * a.List3-15の書き換え
  */
 
 #include<iostream>
@@ -16,32 +16,44 @@ using namespace std;
 
 int main()
 {
-	//整数の個数を入力
-	int n;
-	cout << "整数を加算します。\n";
-	cout << "何個加算しますか : ";
-	cin >> n;
+	//加算する整数の個数
+	int integerNumber;
 
-	//平均を求めるため、int型変数ではなくdouble型変数を初期化
-    double sum = 0;
+	//個数入力を促す
+	cout << "整数を加算します。\n" << "何個加算しますか : ";
 
-    //平均をdouble型変数aveで表す
-    double ave;
+	//個数入力
+	cin >> integerNumber;
 
-    //n回整数入力を繰り返す
-    for (int i = 0; i < n; i++){
-    	int t;
+	//合計値
+    double entireSum = 0;
+
+    //平均値
+    double entireAverage = 0;
+
+    //整数を規定回数入力し、その合計と平均値を計算するループ文
+    for (int firstCounter = 1; firstCounter <= integerNumber; firstCounter++){
+
+    	//加算する整数値
+    	int integerValue;
+
+    	//整数値の入力を促す
     	cout << "整数 (0で終了) : ";
-    	cin >> t;
+
+    	//整数入力
+    	cin >> integerValue;
+
     	//0が入力された場合終了
-    	if (t == 0) break;
+    	if (integerValue == 0) {break;}
+
     	//合計に入力値を加算
-    	sum += t;
+    	entireSum += integerValue;
+
     	//平均を計算
-    	ave = sum / i;
+    	entireAverage = entireSum / firstCounter;
     }
     //合計を表示
-    cout << "合計は" << sum << "です。\n";
+    cout << "合計は" << entireSum << "です。\n";
     //平均を表示
-    cout << "平均は" << ave << "です。\n";
+    cout << "平均は" << entireAverage << "です。\n";
 }
