27ac648fdceb535ad684613a2fa9a76b1fc41bcd
ddt-sawa <ddt.sawa@gmail.com>
2018-07-06 16:54:37 +0900

    mod text, 3-26b 変数名・コメント・行内複数ステートメント修正

--- 3-26b/3-26b.cpp
+++ 3-26b/3-26b.cpp
@@ -13,58 +13,110 @@
 #include<ctime>
 #include<cstdlib>
 #include<iostream>
-//string型変数を使用するため、<string>ヘッダを宣言
+//stringクラスを使用するため、<string>ヘッダを宣言
 #include<string>
 
 using namespace std;
 
 int main()
 {
-	//rand関数を使う準備
+	//乱数テーブルを準備
 	srand(time(NULL));
 
-	//人間の勝利回数をwin、敗北回数をloseで管理する
-	int win = 0; int lose = 0;
+	//人間の勝利回数
+	int userWin = 0;
 
-	//winかloseの値が3以上になるまでジャンケンを繰り返す
-     do{
+    //人間の敗北回数
+	int userLose = 0;
 
-        //人間の手をnとする
-        int human;
 
-	    do{
-        cout << "あなたの手を選んでください。(0 =『グー』 1 =『チョキ』 2 =『パー』) : "; cin >> human;
-	    }while (human < 0 || human > 2);
+	//人間とコンピューターでじゃんけんをするループ文
+	do{
 
-	    //コンピューターの手をhandとする
-	    int hand = rand() % 3;
+	        //人間の手
+	        int humanHand;
 
-	    //コンピュータの手と人間の手で場合分け
-         if (human == 0){
-             switch (hand){
-             case 0 : cout << "コンピュータの手はグー、あいこです。\n";           break;
-             case 1 : cout << "コンピュータの手はチョキ、あなたの勝ちです。\n"; win++; break;
-             case 2 : cout << "コンピュータの手はパー、あなたの負けです。\n";  lose++; break;
+	        //人間の手を決めさせるためのループ文
+		    do{
+
+		        //人間の手を0から2の入力値で決めることを告知
+		        cout << "あなたの手を選んでください。(0 =『グー』 1 =『チョキ』 2 =『パー』) : ";
+
+		        //手を入力
+		        cin >> humanHand;
+
+		        //入力値が0,1,2だった場合ループを抜ける
+		        }while (humanHand < 0 || humanHand > 2);
+
+		    //コンピューターの手(0から2の乱数)
+		    int computerHand = rand() % 3;
+
+		    //人間の手が0 =『グー』の場合
+         if (humanHand == 0){
+             switch (computerHand){
+
+             case 0 : cout << "コンピュータの手はグー、あいこです。\n";
+             break;
+
+             case 1 : cout << "コンピュータの手はチョキ、あなたの勝ちです。\n";
+             userWin++;  //勝利回数+1
+             break;
+
+             case 2 : cout << "コンピュータの手はパー、あなたの負けです。\n";
+             userLose++; //敗北回数+1
+             break;
              }
          }
-         else if (human == 1){
-            switch (hand){
-            case 0 : cout << "コンピュータの手はグー、あなたの負けです。\n"; lose++; break;
-	        case 1 : cout << "コンピュータの手はチョキ、あいこです。\n" ;   break;
-    	    case 2 : cout << "コンピュータの手はパー、あなたの勝ちです。\n"; win++; break;
+
+         //人間の手が1 =『チョキ』の場合
+         else if (humanHand == 1){
+            switch (computerHand){
+
+            case 0 : cout << "コンピュータの手はグー、あなたの負けです。\n";
+            userLose++;  //敗北回数+1
+            break;
+
+	        case 1 : cout << "コンピュータの手はチョキ、あいこです。\n" ;
+	        break;
+
+    	    case 2 : cout << "コンピュータの手はパー、あなたの勝ちです。\n";
+    	    userWin++;   //勝利回数+1
+    	    break;
             }
          }
 
+         //人間の手が2 =『パー』の場合
 	     else {
-	        switch (hand){
-	        case 0 : cout << "コンピュータの手はグー、あなたの勝ちです。\n"; win++; break;
-		    case 1 : cout << "コンピュータの手はチョキ、あなたの負けです。\n";lose++; break;
-		    case 2 : cout << "コンピュータの手はパー、あいこです。\n"; break;
+	        switch (computerHand){
+
+	        case 0 : cout << "コンピュータの手はグー、あなたの勝ちです。\n";
+	        userWin++;   //勝利回数+1
+	        break;
+
+		    case 1 : cout << "コンピュータの手はチョキ、あなたの負けです。\n";
+		    userLose++;  //敗北回数+1
+		    break;
+
+		    case 2 : cout << "コンピュータの手はパー、あいこです。\n";
+		    break;
 		    }
 	     }
-cout << "現在" << win << "勝" << lose << "敗です。\n";
- }while (win < 3 && lose < 3);
+         //現在の勝敗を告知
+         cout << "現在" << userWin << "勝" << userLose << "敗です。\n";
 
-if (win == 3) cout << "おめでとうございます、あなたの勝利です。\n";
-else cout << "残念ながら、あなたの敗北です。\n";
+         //どちらかが3勝するまでじゃんけんループ
+     }while (userWin < 3 && userLose < 3);
+
+	//人間が3勝した場合
+    if (userWin == 3) {
+
+    	//勝利を告知
+    	cout << "おめでとうございます、あなたの勝利です。\n";
+    }
+
+    //人間が3敗した場合
+    else {
+    	//敗北を告知
+    	cout << "残念ながら、あなたの敗北です。\n";
+    }
 }
