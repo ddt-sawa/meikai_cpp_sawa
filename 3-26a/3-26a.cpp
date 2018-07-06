f4d9a4219a4957e327be2bd29558cfb8e0b73ede
ddt-sawa <ddt.sawa@gmail.com>
2018-07-06 16:07:37 +0900

    mod text, 3-26a 変数名・コメント・行内複数ステートメント修正

--- 3-26a/3-26a.cpp
+++ 3-26a/3-26a.cpp
@@ -14,57 +14,85 @@
 #include<ctime>
 #include<cstdlib>
 #include<iostream>
-//string型変数を使用するため、<string>ヘッダを宣言
+//stringクラスを使用するため、<string>ヘッダを宣言
 #include<string>
 
 using namespace std;
 
 int main()
 {
-	//rand関数を使う準備
+	//乱数テーブルを準備
 	srand(time(NULL));
-    //もう一度ジャンケンするかを尋ねるstring型変数retryを宣言
-	string retry;
 
+    //再試行するかを判断する文字列
+	string inputRetry;
+
+	//人間とコンピューターでじゃんけんをするループ文
      do{
 
-        //人間の手をhumanとする
-        int human;
+        //人間の手
+        int humanHand;
 
+        //人間の手を決めさせるためのループ文
 	    do{
-	    cout << "あなたの手を選んでください。(0 =『グー』 1 =『チョキ』 2 =『パー』) : "; cin >> human;
-	    }while (human < 0 || human > 2);
 
-	    //コンピューターの手をhandとする
-	    int hand = rand() % 3;
+	    //人間の手を0から2の入力値で決めることを告知
+	    cout << "あなたの手を選んでください。(0 =『グー』 1 =『チョキ』 2 =『パー』) : ";
 
-	    //コンピュータの手と人間の手で場合分け
-         if (human == 0){
-             switch (hand){
-             case 0 : cout << "コンピュータの手はグー、あいこです。\n";           break;
-             case 1 : cout << "コンピュータの手はチョキ、あなたの勝ちです。\n"; break;
-             case 2 : cout << "コンピュータの手はパー、あなたの負けです。\n";   break;
+	    //手を入力
+	    cin >> humanHand;
+
+	    //入力値が0,1,2だった場合ループを抜ける
+	    }while (humanHand < 0 || humanHand > 2);
+
+	    //コンピューターの手(0から2の乱数)
+	    int conputerHand = rand() % 3;
+
+	    //人間の手が0 =『グー』の場合
+         if (humanHand == 0){
+        	  //コンピューターの手で場合分け
+             switch (conputerHand){
+             case 0 : cout << "コンピュータの手はグー、あいこです。\n";
+             break;
+             case 1 : cout << "コンピュータの手はチョキ、あなたの勝ちです。\n";
+             break;
+             case 2 : cout << "コンピュータの手はパー、あなたの負けです。\n";
+             break;
              }
          }
-         else if (human == 1){
-            switch (hand){
-            case 0 : cout << "コンピュータの手はグー、あなたの負けです。\n"; break;
-	        case 1 : cout << "コンピュータの手はチョキ、あいこです。\n" ;   break;
-    	    case 2 : cout << "コンピュータの手はパー、あなたの勝ちです。\n";break;
+
+         //人間の手が1 =『チョキ』の場合
+         else if (humanHand == 1){
+        	  //コンピューターの手で場合分け
+            switch (conputerHand){
+            case 0 : cout << "コンピュータの手はグー、あなたの負けです。\n";
+            break;
+	        case 1 : cout << "コンピュータの手はチョキ、あいこです。\n";
+	        break;
+    	    case 2 : cout << "コンピュータの手はパー、あなたの勝ちです。\n";
+    	    break;
             }
          }
 
+         //人間の手が2 =『パー』の場合
 	     else {
-	        switch (hand){
-	        case 0 : cout << "コンピュータの手はグー、あなたの勝ちです。\n"; break;
-		    case 1 : cout << "コンピュータの手はチョキ、あなたの負けです。\n" ; break;
-		    case 2 : cout << "コンピュータの手はパー、あいこです。\n"; break;
+	    	  //コンピューターの手で場合分け
+	        switch (conputerHand){
+	        case 0 : cout << "コンピュータの手はグー、あなたの勝ちです。\n";
+	        break;
+		    case 1 : cout << "コンピュータの手はチョキ、あなたの負けです。\n" ;
+		    break;
+		    case 2 : cout << "コンピュータの手はパー、あいこです。\n";
+		    break;
 		    }
 	     }
      //もう一度じゃんけんをするか尋ねる
-    cout << " もう一回やりますか？　(『はい』= Y 『いいえ』 = N)\n";
-    //retryに'Y'か'y'が入力された場合もう一度
-    cin >> retry;
-     }while (retry == "Y" || retry == "y");
+    cout << " もう一回やりますか？ (『はい』= Y 『いいえ』 = N)\n";
+
+    //文字列入力
+    cin >> inputRetry;
+
+    //'Y'か'y'が入力された場合、もう一度じゃんけん
+     }while (inputRetry == "Y" || inputRetry == "y");
 }
 
