2ddd8e2a03e5dca8a24c72c9440f72a06215325c
ddt-sawa <ddt.sawa@gmail.com>
2018-07-06 18:05:11 +0900

    mod text, 4-12　変数名・コメント・行内複数ステートメント修正

--- 4-12/4-12.cpp
+++ 4-12/4-12.cpp
@@ -14,29 +14,43 @@ using namespace std;
 
 int main()
 {
-	//列挙体の宣言と、マーク入力に使う変数の宣言
-	enum trump {diamond, heart, club, spade, invalid};
-	int type;
+	//トランプのマークを格納した列挙体の宣言
+	enum trumpMark {diamondMark, heartMark, clubMark, spadeMark, invalidMark};
 
-	//マークを入力
+	//マーク選択に使う整数値
+	int userChoice;
+
+	//マークを入力させるループ文
 	do {
+		//マークに対応した整数値の入力を促す
 		cout << "0 -『ダイヤ』  1 -『ハート』  2 -『クラブ』 3 -『スペード』 4 -『終了』";
-		cin >> type;
-		//列挙子内のマーク入力まで繰り返し
-		} while (type < diamond || type > invalid);
 
-	//列挙子がinvalidでないとき
-	if (type != invalid){
+		//整数値入力
+		cin >> userChoice;
 
-	    // trump型変数にtypeをキャスト
-		trump symbol = static_cast<trump>(type);
+		//値が列挙子内のマークを指すまで繰り返し
+		} while (userChoice < diamondMark || userChoice > invalidMark);
 
-		//symbol毎にswitch文で分岐
-		switch (symbol){
-		case diamond : cout << "ダイアを出しました。\n"; break;
-		case heart : cout << "ハートを出しました。\n"; break;
-		case club : cout << "クラブを出しました。\n"; break;
-		case spade : cout << "スペードを出しました。\n"; break;
+	//選んだ列挙子が4 = 『invalidMark』でないとき
+	if (userChoice != invalidMark){
+
+	    // trumpMark型変数に選んだ値ををキャスト
+		trumpMark userSymbol = static_cast<trumpMark>(userChoice);
+
+		//選んだマーク毎に表示文を変更
+		switch (userSymbol){
+
+		case diamondMark : cout << "ダイヤを出しました。\n"; //0 = 『ダイヤ』
+		break;
+
+		case heartMark : cout << "ハートを出しました。\n";   //1 = 『ハート』
+		break;
+
+		case clubMark : cout << "クラブを出しました。\n";    //2 = 『クラブ』
+		break;
+
+		case spadeMark : cout << "スペードを出しました。\n"; //3 = 『スペード』
+		break;
 		}
 	}
 }
