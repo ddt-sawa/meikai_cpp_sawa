09404c4d26d6fdc271478950e8aea004aafced97
ddt-sawa <ddt.sawa@gmail.com>
2018-07-10 20:56:38 +0900

    mod text, 2-16 インデント適用

--- 2-16/2-16.cpp
+++ 2-16/2-16.cpp
@@ -20,39 +20,39 @@ int main()
 	//整数値の入力
 	cout << "月を入力してください : "; cin >> thisMonth;
 
-	 //入力値を利用したswitch文の作成
+	//入力値を利用したswitch文の作成
 	switch (thisMonth){
 
 
-	      //(12)1,2月の場合"冬"と表示
-	      case 1 :
-	      case 2 : {cout << "冬\n";}
-	      break;
+	//(12)1,2月の場合"冬"と表示
+	case 1 :
+	case 2 : {cout << "冬\n";}
+	break;
 
-	      //3,4,5月の場合"春"と表示
-	      case 3 :
-	      case 4 :
-	      case 5 : {cout << "春\n";}
-	      break;
+	//3,4,5月の場合"春"と表示
+	case 3 :
+	case 4 :
+	case 5 : {cout << "春\n";}
+	break;
 
-	      //6,7,8月の場合"夏"と表示
-	      case 6 :
-	      case 7 :
-	      case 8 : {cout << "夏\n";}
-	      break;
+	//6,7,8月の場合"夏"と表示
+	case 6 :
+	case 7 :
+	case 8 : {cout << "夏\n";}
+	break;
 
-	      //9,10,11月の場合"秋"と表示
-	      case 9 :
-	      case 10:
-	      case 11: {cout << "秋\n";}
-	      break;
+	//9,10,11月の場合"秋"と表示
+	case 9 :
+	case 10:
+	case 11: {cout << "秋\n";}
+	break;
 
-	      //12月の場合"冬"と表示
-	      case 12: {cout << "冬\n";}
-	      break;
+	//12月の場合"冬"と表示
+	case 12: {cout << "冬\n";}
+	break;
 
-	      //上記以外の整数値が入力された場合、"その月はありません。"と表示
-	      default: {cout << "その月はありません。\n";}
-	      break;
-	     }
+	//上記以外の整数値が入力された場合、"その月はありません。"と表示
+	default: {cout << "その月はありません。\n";}
+	break;
+	}
 }
