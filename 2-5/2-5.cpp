1d491f9b6f07366d5655609bd88e10db2e711726
ddt-sawa <ddt.sawa@gmail.com>
2018-07-10 20:45:18 +0900

    mod text, 2-5 インデント適用

--- 2-5/2-5.cpp
+++ 2-5/2-5.cpp
@@ -16,38 +16,38 @@ using namespace std;
 
 int main()
 {
-	 //3で割り切れるか調べたい整数を宣言
-	 int firstNumber;
+	//3で割り切れるか調べたい整数を宣言
+	int firstNumber;
 
-	 //整数値の入力を促す
-	 cout << "整数値 : ";
+	//整数値の入力を促す
+	cout << "整数値 : ";
 
-	 //整数値の入力
-	 cin >> firstNumber;
+	//整数値の入力
+	cin >> firstNumber;
 
-	 //整数が正の場合
-	 if (firstNumber > 0){
+	//整数が正の場合
+	if (firstNumber > 0){
 
-		 //3で割り切れる場合
-		 if (firstNumber % 3 == 0){
-			 cout << "その値は3で割り切れます。\n";
-		 }
+		//3で割り切れる場合
+		if (firstNumber % 3 == 0){
+			cout << "その値は3で割り切れます。\n";
+		}
 
-	     //3で割ると1余る場合
-		 else if (firstNumber % 3 == 1){
-			 cout << "その値を3で割った剰余は1です。\n";
-		 }
+		//3で割ると1余る場合
+		else if (firstNumber % 3 == 1){
+			cout << "その値を3で割った剰余は1です。\n";
+		}
 
-	     //3で割ると2余る場合
-		 else {
-			 cout << "その値を3で割った剰余は2です。\n";
-		 }
-	 }
+		//3で割ると2余る場合
+		else {
+			cout << "その値を3で割った剰余は2です。\n";
+		}
+	}
 
-	 //整数が負及び0の場合
-	 else{
-		 cout << "正でない値が入力されました。\n";
-     }
+	//整数が負及び0の場合
+	else{
+		cout << "正でない値が入力されました。\n";
+	}
 
 }
 
