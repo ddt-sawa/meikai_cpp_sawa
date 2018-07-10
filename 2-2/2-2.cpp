d7ff9574b14e2db6cca1b3832fde0202a7e2814a
ddt-sawa <ddt.sawa@gmail.com>
2018-07-10 20:43:45 +0900

    mod text, 2-2 インデント適用

--- 2-2/2-2.cpp
+++ 2-2/2-2.cpp
@@ -15,28 +15,28 @@ using namespace std;
 int main()
 {
 	//符号を調べたい整数値の宣言
-    int firstNumber;
+	int firstNumber;
 
-    //整数値の入力を促す
-    cout << "整数値 : ";
+	//整数値の入力を促す
+	cout << "整数値 : ";
 
-    //整数値を読み込む
-    cin >> firstNumber;
+	//整数値を読み込む
+	cin >> firstNumber;
 
-    //整数値が正の場合
-    if (firstNumber > 0) {
-    	cout << "その値は正です。\n";
-    }
+	//整数値が正の場合
+	if (firstNumber > 0) {
+		cout << "その値は正です。\n";
+	}
 
-    //整数値が負の場合
-    else if (firstNumber < 0){
-    	cout << "その値は負です。\n";
-    }
+	//整数値が負の場合
+	else if (firstNumber < 0){
+		cout << "その値は負です。\n";
+	}
 
-    //整数値が0で、問題文によりelseではなくelse ifを用いた場合
-    else if (firstNumber == 0) {
-    	cout << "その値は0です。\n";
-    }
+	//整数値が0で、問題文によりelseではなくelse ifを用いた場合
+	else if (firstNumber == 0) {
+		cout << "その値は0です。\n";
+	}
 }
 
 //コンパイルエラーは表示されない。
