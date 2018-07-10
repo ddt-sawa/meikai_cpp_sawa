9b6e27cf3d0fb74c95caed673666ad137c9d7cf1
ddt-sawa <ddt.sawa@gmail.com>
2018-07-10 20:32:05 +0900

    mod text, 1-6 インデント適用

--- 1-6.cpp
+++ 1-6.cpp
@@ -10,27 +10,27 @@ using namespace std;
 // main関数の利用
 int main()
 {
-   //xに代入する値をfirstNumberとする
-    int firstNumber;
+	//xに代入する値をfirstNumberとする
+	int firstNumber;
 
-    //yに代入する値をsecondNumberとする
-    int secondNumber;
+	//yに代入する値をsecondNumberとする
+	int secondNumber;
 
-    //int型変数firstNumberに対し、課題確認のためあえて小数部を持つ実数値を代入
-    firstNumber = 63.5;
+	//int型変数firstNumberに対し、課題確認のためあえて小数部を持つ実数値を代入
+	firstNumber = 63.5;
 
-    //int型変数firstNumberに対し、課題確認のためあえて.0の小数表記を持つ実数値を代入
-    secondNumber = 18.0;
+	//int型変数firstNumberに対し、課題確認のためあえて.0の小数表記を持つ実数値を代入
+	secondNumber = 18.0;
 
-    //firstNumberの表示において小数部切捨てを確認
-    cout << "xの値は" << firstNumber << "です。\n";
-    //secondNumberの表示において小数部切捨てを確認
-    cout << "yの値は" << secondNumber << "です。\n";
+	//firstNumberの表示において小数部切捨てを確認
+	cout << "xの値は" << firstNumber << "です。\n";
+	//secondNumberの表示において小数部切捨てを確認
+	cout << "yの値は" << secondNumber << "です。\n";
 
-    //両変数の合計において小数部切捨てを確認
-    cout << "合計は" << firstNumber + secondNumber << "です。\n";
-    //両変数の平均において少数部切捨てを確認
-    cout << "平均は" << (firstNumber + secondNumber) / 2 << "です。\n";
+	//両変数の合計において小数部切捨てを確認
+	cout << "合計は" << firstNumber + secondNumber << "です。\n";
+	//両変数の平均において少数部切捨てを確認
+	cout << "平均は" << (firstNumber + secondNumber) / 2 << "です。\n";
 }
 
 //int型変数の評価において、小数部が切り捨てられることを確認
