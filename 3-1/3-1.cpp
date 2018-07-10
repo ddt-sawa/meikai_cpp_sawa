36a0860533db9fb2da7450209d362fafa750df77
ddt-sawa <ddt.sawa@gmail.com>
2018-07-10 20:57:48 +0900

    mod text, 3-1 インデント適用

--- 3-1/3-1.cpp
+++ 3-1/3-1.cpp
@@ -16,45 +16,45 @@ using namespace std;
 
 int main()
 {
-    //再試行を行うかどうかを判定する文字列の宣言
+	//再試行を行うかどうかを判定する文字列の宣言
 	string userRetry;
 
-    //入力者が望む回数、符号判定を繰り返す
-    do {
+	//入力者が望む回数、符号判定を繰り返す
+	do {
 
-    	// 符号を判定したい整数の宣言
-    	int firstNumber;
+		// 符号を判定したい整数の宣言
+		int firstNumber;
 
-    	//整数の入力
-    	cout << "整数値 : ";  cin >> firstNumber;
+		//整数の入力
+		cout << "整数値 : ";  cin >> firstNumber;
 
-    	//整数が正の場合
-    	if (firstNumber > 0){
+		//整数が正の場合
+		if (firstNumber > 0){
 
-    		//値が正であることを表示
-    		cout << "その値は正です\n";
-    	}
+			//値が正であることを表示
+			cout << "その値は正です\n";
+		}
 
-    	//整数が負の場合
-    	else if (firstNumber < 0){
+		//整数が負の場合
+		else if (firstNumber < 0){
 
-    		//値が負であることを表示
-    		cout << "その値は負です\n";
-    	}
+			//値が負であることを表示
+			cout << "その値は負です\n";
+		}
 
-    	//整数が0の場合
-    	else{
+		//整数が0の場合
+		else{
 
-    		//値が0であることを表示
-    		cout << "その値は0です。\n";
-    	}
+			//値が0であることを表示
+			cout << "その値は0です。\n";
+		}
 
-    	//再試行するかを訪ねる
-    	cout << "もう一度？ Y・・・Yes / N・・・No";
+		//再試行するかを訪ねる
+		cout << "もう一度？ Y・・・Yes / N・・・No";
 
-    	//YesorNoの入力
-    	cin >> userRetry;
+		//YesorNoの入力
+		cin >> userRetry;
 
-	//'Y','y'が入力された場合再試行
-    }while(userRetry == "Y" || userRetry == "y");
+		//'Y','y'が入力された場合再試行
+	}while(userRetry == "Y" || userRetry == "y");
 }
