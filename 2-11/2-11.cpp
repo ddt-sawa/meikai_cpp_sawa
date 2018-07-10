132e1f856fd7af9c9ff88ef2f5cf9af3112836c6
ddt-sawa <ddt.sawa@gmail.com>
2018-07-10 20:52:09 +0900

    mod text, 2-11 インデント適用

--- 2-11/2-11.cpp
+++ 2-11/2-11.cpp
@@ -15,25 +15,25 @@ using namespace std;
 int main()
 {
 	//三つの整数を宣言
-    int firstNumber, secondNumber, thirdNumber;
+	int firstNumber, secondNumber, thirdNumber;
 
-    cout << "整数1 : "; cin >> firstNumber;     //整数1の入力
-    cout << "整数2 : "; cin >> secondNumber;    //整数2の入力
-    cout << "整数3 : "; cin >> thirdNumber;     //整数3の入力
+	cout << "整数1 : "; cin >> firstNumber;     //整数1の入力
+	cout << "整数2 : "; cin >> secondNumber;    //整数2の入力
+	cout << "整数3 : "; cin >> thirdNumber;     //整数3の入力
 
-    //初めに整数を1を中央値(medValue)として、中央値でなかった場合入れ替え
-    int medValue = firstNumber;
+	//初めに整数を1を中央値(medValue)として、中央値でなかった場合入れ替え
+	int medValue = firstNumber;
 
-    //整数2が中央値の場合、medValueに代入
-    if ((secondNumber <= firstNumber && secondNumber >= thirdNumber) || (secondNumber <= thirdNumber && secondNumber >= firstNumber)){
-    	medValue = secondNumber;
-    }
+	//整数2が中央値の場合、medValueに代入
+	if ((secondNumber <= firstNumber && secondNumber >= thirdNumber) || (secondNumber <= thirdNumber && secondNumber >= firstNumber)){
+		medValue = secondNumber;
+	}
 
-    //整数3が中央値の場合、medValueに代入
-    else if  ((thirdNumber <= firstNumber && thirdNumber >= secondNumber) || (thirdNumber <= secondNumber && thirdNumber >= firstNumber)) {
-    	medValue = thirdNumber;
-    }
+	//整数3が中央値の場合、medValueに代入
+	else if  ((thirdNumber <= firstNumber && thirdNumber >= secondNumber) || (thirdNumber <= secondNumber && thirdNumber >= firstNumber)) {
+		medValue = thirdNumber;
+	}
 
-    //中央値の表示
-    cout << "中央値は" <<  medValue <<"です。\n";
+	//中央値の表示
+	cout << "中央値は" <<  medValue <<"です。\n";
 }
