7d5ffdaea92aadf92539ff1c590136875190b6e2
ddt-sawa <ddt.sawa@gmail.com>
2018-07-10 20:54:28 +0900

    mod text, 2-13 インデント適用・変数名修正

--- 2-13/2-13.cpp
+++ 2-13/2-13.cpp
@@ -16,24 +16,24 @@ int main()
 	//2整数を宣言
 	int firstNumber, secondNumber;
 
-    //
-    cout << "整数x : "; cin >> firstNumber;    //整数xを入力
-    cout << "整数y : "; cin >> secondNumber;   //整数yを入力
+	//
+	cout << "整数x : "; cin >> firstNumber;    //整数xを入力
+	cout << "整数y : "; cin >> secondNumber;   //整数yを入力
 
-    //整数xが整数yより小さいならば、値を交換
-    if (firstNumber < secondNumber){
+	//整数xが整数yより小さいならば、値を交換
+	if (firstNumber < secondNumber){
 
-    	int t = firstNumber;           //整数xの値を一時保存
-    	firstNumber = secondNumber;    //整数yの値を整数xに代入
-    	secondNumber = t;              //保存していた整数xの値を整数yに代入
-    }
+		int temporaryNumber = firstNumber;           //整数xの値を一時保存
+		firstNumber = secondNumber;                  //整数yの値を整数xに代入
+		secondNumber = temporaryNumber;              //保存していた整数xの値を整数yに代入
+	}
 
-    //2整数を降順にソートしたことを表示
-    cout << "整数x <= 整数y となるようにソートしました。\n";
+	//2整数を降順にソートしたことを表示
+	cout << "整数x <= 整数y となるようにソートしました。\n";
 
-    //整数xの表示
-    cout << "整数xは" << firstNumber << "です。\n";
+	//整数xの表示
+	cout << "整数xは" << firstNumber << "です。\n";
 
-    //整数yの表示
-    cout << "整数yは" << secondNumber << "です。\n";
+	//整数yの表示
+	cout << "整数yは" << secondNumber << "です。\n";
 }
