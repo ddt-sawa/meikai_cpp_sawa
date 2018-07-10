52412b303a92aaf64a9c292ef4ff89d98144031f
ddt-sawa <ddt.sawa@gmail.com>
2018-07-10 20:40:18 +0900

    mod text, 1-12 インデント適用・変数名・コメント修正

--- 1-12.cpp
+++ 1-12.cpp
@@ -14,21 +14,21 @@ using namespace std;
 
 int main()
 {
-	//底辺をbase、高さをheightとする
-	double base, height;
+	// 底辺をbase、高さをheightとする
+	double triangularBase, triangularHeight;
 
 	// 底辺の値入力を促す
 	cout << "三角形の底辺 : ";
 
-	// xに実数値を読み込む
-	cin >> base;
+	// 実数値を読み込む
+	cin >> triangularBase;
 
 	// 高さの値入力を促す
 	cout << "三角形の高さ : ";
 
-	// heightに実数値を読み込む
-	cin >> height;
+	// 実数値を読み込む
+	cin >> triangularHeight;
 
 	// 三角形の面積を表示
-    cout << "面積は" << base * height / 2 << "です。\n";
+	cout << "面積は" << triangularBase * triangularHeight / 2 << "です。\n";
 }
