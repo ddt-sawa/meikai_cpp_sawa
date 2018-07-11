925fba1bb1cd1e2820db0de1231e896f97ceade8
ddt-sawa <ddt.sawa@gmail.com>
2018-07-11 10:56:50 +0900

    mod text, 3-23b インデント適用・合計値の型変更・コメント・前置演算子修正

--- 3-23b/3-23b.cpp
+++ 3-23b/3-23b.cpp
@@ -11,48 +11,52 @@ using namespace std;
 int main()
 {
 	//加算する整数の個数
-		int integerNumber;
+	int integerNumber;
 
-		//個数入力を促す
-		cout << "整数を加算します。\n" << "何個加算しますか : ";
+	//個数入力を促す
+	cout << "整数を加算します。\n" << "何個加算しますか : ";
 
-		//個数入力
-		cin >> integerNumber;
+	//個数入力
+	cin >> integerNumber;
 
-		//合計値
-	    double entireSum = 0;
+	//合計値
+	int entireSum = 0;
 
-	    //平均値
-	    double entireAverage = 0;
+	//平均値
+	double entireAverage = 0;
 
-	    //整数を規定回数入力し、その合計と平均値を計算するループ文
-	    for (int firstCounter = 1; firstCounter <= integerNumber; firstCounter++){
+	//整数を規定回数入力し、その合計と平均値を計算するループ文
+	for (int firstCounter = 1; firstCounter <= integerNumber; ++firstCounter){
 
-	    	//加算する整数値
-	    	int integerValue;
+		//加算する整数値
+		int integerValue;
 
-	    	//整数値の入力を促す
-	    	cout << "整数 : ";
+		//整数値の入力を促す
+		cout << "整数 : ";
 
-	    	//整数入力
-	    	cin >> integerValue;
+		//整数入力
+		cin >> integerValue;
 
-	    	//合計が1000を超えた場合終了
-	    	if (entireSum + integerValue > 1000){
-	    		cout << "合計が1000を超えました。\n最後の数値は無視します。\n";
-	    	    break;
+		//合計が1000を超えた場合
+		if (entireSum + integerValue > 1000){
 
-	    	}
-	    	//合計に入力値を加算
-	    	entireSum += integerValue;
+			//合計が1000を超えたことを告知
+			cout << "合計が1000を超えました。\n最後の数値は無視します。\n";
 
-	    	//平均を計算
-	    	entireAverage = entireSum / firstCounter;
-	    }
-	    //合計を表示
-	    cout << "合計は" << entireSum << "です。\n";
-	    //平均を表示
-	    cout << "平均は" << entireAverage << "です。\n";
+			//入力終了
+			break;
+		}
+
+		//合計に入力値を加算
+		entireSum += integerValue;
+
+		//平均を計算
+		entireAverage = static_cast<double>(entireSum) / firstCounter;
 	}
+	//合計を表示
+	cout << "合計は" << entireSum << "です。\n";
+	//平均を表示
+	cout << "平均は" << entireAverage << "です。\n";
+}
 
 
