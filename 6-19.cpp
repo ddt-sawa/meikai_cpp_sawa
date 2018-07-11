5b162a737e30c2f2fa94874d779a4cdd052af40d
ddt-sawa <ddt.sawa@gmail.com>
2018-07-11 19:16:37 +0900

    add text, 6-19

---
+++ 6-19.cpp
@@ -0,0 +1,61 @@
+/*
+ * 6-19.cpp
+ *
+ *  Created on: 2018/06/24
+ *      Author: syuka
+ */
+
+/*演習6-19 List6-21の関数rを、不正な添え字に対して安全に動作するものに書き換えよ。
+ *静的記憶域期間をもつint型の変数を関数内部で定義して、idxが0以上a_size未満でなければ、
+ *その変数への参照を返却すること
+ */
+
+#include<iostream>
+
+using namespace std;
+
+//配列の要素数
+const int arraySize = 5;
+
+//関数名         : returnArray
+//仕様           : 配列の要素への参照を返却する関数
+//引数           : int型変数arrayIndex(配列の添え字)
+//返り値         : int型変数intArray[arrayIndex]への参照
+//作成者         : sawa
+//日付           : 7/11
+int& returnArray(int arrayIndex)
+{
+	//不正な添え字を受け取った場合に返却する数値
+	static int safetyValue;
+
+	//添え字が0未満もしくは要素数以上だった場合
+	if (arrayIndex < 0 || arrayIndex > arraySize) {
+
+		//仮の数値(0)を返す
+		return safetyValue;
+	}
+
+	//配列の宣言
+	static int intArray[arraySize];
+
+	//添え字によって指定された配列の要素を返却
+	return intArray[arrayIndex];
+}
+
+
+int main()
+{
+	//関数内の配列要素に値を走査代入するループ
+	for (int firstCounter = 0; firstCounter < arraySize; ++firstCounter) {
+
+		//添え字と同じ値を要素に代入
+		returnArray(firstCounter) = firstCounter;
+	}
+
+	//関数内の配列要素を走査表示するループ
+	for (int firstCounter = 0; firstCounter < arraySize; ++firstCounter) {
+
+		//関数を呼び出して、配列要素を表示
+		cout << "returnArray(" << firstCounter << ") = " << returnArray(firstCounter) << '\n';
+	}
+}
