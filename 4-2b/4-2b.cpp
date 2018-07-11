b9eaaf24fd25861a4df597595404c69b5701d5c3
ddt-sawa <ddt.sawa@gmail.com>
2018-07-11 11:17:02 +0900

    mod text, 4-2b インデント適用・前置演算子修正

--- 4-2b/4-2b.cpp
+++ 4-2b/4-2b.cpp
@@ -16,14 +16,14 @@ using namespace std;
 int main()
 {
 	//unsigned int型の最小値から最大値まで表示
-    for (unsigned CounterVariable = 0; ; CounterVariable++){
+	for (unsigned CounterVariable = 0; ; ++CounterVariable){
 
-    	//8進数で表示
-    	cout << oct << CounterVariable << '\n';
-    	//10進数で表示
-        cout << dec << CounterVariable << '\n';
-        //16進数で表示
-        cout << hex << CounterVariable << '\n';
-       }
+		//8進数で表示
+		cout << oct << CounterVariable << '\n';
+		//10進数で表示
+		cout << dec << CounterVariable << '\n';
+		//16進数で表示
+		cout << hex << CounterVariable << '\n';
+	}
 
 }
