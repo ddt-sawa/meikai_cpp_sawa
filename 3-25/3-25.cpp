cd30c3cabd0479848deea101b75ef330370c0892
ddt-sawa <ddt.sawa@gmail.com>
2018-07-11 11:02:39 +0900

    mod text, 3-25 インデント適用・goto文・宣言の順番・前置演算子修正

--- 3-25/3-25.cpp
+++ 3-25/3-25.cpp
@@ -19,18 +19,8 @@ using namespace std;
 
 int main()
 {
-	//乱数テーブルを準備
-	srand(time(NULL));
-
-	//0-99までの乱数を正解として設定
-	int randomNumber = rand() % 100;
-
-	//回答値
-	int userAnswer;
-
 	//制限回数
 	int answerLimit;
-
 
 	//制限回数の入力を促す
 	cout << "数当てゲーム開始!\n" << "制限回数を指定してください。";
@@ -41,11 +31,23 @@ int main()
 	//回答入力を促す
 	cout << "0-99の数を当ててください。\n";
 
-	//正解が出るか、制限回数が来るまで入力を続けるループ文
-	for (int firstCounter = 0; firstCounter < answerLimit; firstCounter++){
+	//乱数テーブルを準備
+	srand(time(NULL));
 
-		//回答を促し、値を読み込む
-		cout << "いくつでしょうか。";  cin >> userAnswer;
+	//0-99までの乱数を正解として設定
+	int randomNumber = rand() % 100;
+
+	//正解が出るか、制限回数が来るまで入力を続けるループ文
+	for (int firstCounter = 0; firstCounter < answerLimit; ++firstCounter){
+
+		//回答値
+		int userAnswer;
+
+		//回答を促す
+		cout << "いくつでしょうか。";
+
+		//回答値入力
+		cin >> userAnswer;
 
 		//回答が正解より大きい場合
 		if (userAnswer> randomNumber) {
@@ -61,15 +63,25 @@ int main()
 			cout << "もっと大きな数です。\n";
 		}
 
-		//正解した場合、ループを抜けて最終行に移動
+		//回答が正解だった場合
 		else{
-			cout << "正解です！"; goto gameClear;
+
+			//正解したことを告知
+			cout << "正解です！";
+
+			//ゲーム終了
+			break;
+		}
+
+		//制限回数内に正解できなかった場合
+		if (firstCounter == answerLimit) {
+
+			//正解を告知
+			cout << "正解は" << randomNumber << "でした。\n";
+
+			//ゲーム終了
+			break;
 		}
 	}
 
-	//制限回数内に正解できなかった場合正解告知
-	cout << "正解は" << randomNumber << "でした。\n";
-
-	//正解できた場合ゲーム終了
-	gameClear : ;
 }
