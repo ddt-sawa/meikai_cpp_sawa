4cc69fc9a216220d4c57bbabad91dd3b5a2276d4
ddt-sawa <ddt.sawa@gmail.com>
2018-07-11 11:25:58 +0900

    mod text, 5-4 インデント適用・前置演算子修正

--- 5-4/5-4.cpp
+++ 5-4/5-4.cpp
@@ -20,29 +20,29 @@ int main()
 	// 配列の要素数
 	const int arrayNumber = 6;
 
-    //配列宣言
+	//配列宣言
 	int intArray[arrayNumber];
 
-	//rand関数の準備
+	//乱数テーブルの準備
 	srand(time(NULL));
 
 	//配列に値を走査代入するループ文
-	for (int counterVariable = 0; counterVariable < arrayNumber; counterVariable++){
+	for (int counterVariable = 0; counterVariable < arrayNumber; ++counterVariable){
 
 		//配列に入れる値が連続しないようにするループ文
 		do{
 			//配列に乱数を代入
 			intArray[counterVariable] = rand() % 10 + 1;
 
-		 //配列の添え字が[0]の場合
-		 if (counterVariable == 0) {
-			 //前の数値を参照しないので値を確定してループを抜ける
-			 break;
-		     }
-		 //前の要素と値が一致した場合、再試行
+			//配列の添え字が[0]の場合
+			if (counterVariable == 0) {
+				//前の数値を参照しないので値を確定してループを抜ける
+				break;
+			}
+			//前の要素と値が一致した場合、再試行
 		} while (intArray[counterVariable] == intArray[counterVariable - 1]);
 
 		//配列を走査表示
-	    cout << intArray[counterVariable] << '\n';
+		cout << intArray[counterVariable] << '\n';
 	}
 }
