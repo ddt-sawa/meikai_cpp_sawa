df6da7d217e51f04c2995abf2821c6e2f6e72be8
ddt-sawa <ddt.sawa@gmail.com>
2018-07-11 11:36:02 +0900

    mod text, 5-10 インデント適用・前置演算子修正

--- 5-10.cpp
+++ 5-10.cpp
@@ -30,10 +30,10 @@ int main()
 	cout << "4行3列の配列を入力してください。\n";
 
 	//配列aに要素を入力するループ文
-	for(int firstCounter = 0; firstCounter < 4; firstCounter++){
+	for(int firstCounter = 0; firstCounter < 4; ++firstCounter){
 
 		//列ごとの要素を入力するループ文
-		for(int secondCounter = 0; secondCounter < 3; secondCounter++) {
+		for(int secondCounter = 0; secondCounter < 3; ++secondCounter) {
 
 			//値入力
 			cin >> firstArray[firstCounter][secondCounter];
@@ -44,10 +44,10 @@ int main()
 	cout << "3行4列の配列を入力してください。\n";
 
 	//配列bに要素を入力するループ文
-	for(int firstCounter = 0; firstCounter < 3; firstCounter++){
+	for(int firstCounter = 0; firstCounter < 3; ++firstCounter){
 
 		//列ごとの要素を入力するループ文
-		for(int secondCounter = 0; secondCounter < 4; secondCounter++) {
+		for(int secondCounter = 0; secondCounter < 4; ++secondCounter) {
 
 			//値入力
 			cin >> secondArray[firstCounter][secondCounter];
@@ -57,23 +57,23 @@ int main()
 	//配列の積を計算するループ文
 
 	//回答となる積の行を指定
-	for (int firstCounter = 0; firstCounter < 4; firstCounter++){
+	for (int firstCounter = 0; firstCounter < 4; ++firstCounter){
 
 		//回答となる積の列を指定
-		for(int secondCounter = 0; secondCounter < 4; secondCounter++){
+		for(int secondCounter = 0; secondCounter < 4; ++secondCounter){
 
 			//積の計算をするために、配列aの同じ行の要素、配列bの同じ行の要素を掛け合わせるループ文
-			for (int thirdCounter= 0; thirdCounter < 3; thirdCounter++) {
+			for (int thirdCounter= 0; thirdCounter < 3; ++thirdCounter) {
 
 				//配列aの行の要素×配列bの列の要素を順番に足していく
-		        answerArray[firstCounter][secondCounter] += (firstArray[firstCounter][thirdCounter] * secondArray[thirdCounter][secondCounter]);
-	            }
+				answerArray[firstCounter][secondCounter] += (firstArray[firstCounter][thirdCounter] * secondArray[thirdCounter][secondCounter]);
+			}
 
 			//計算した積を表示
-			  cout << setw(3) << answerArray[firstCounter][secondCounter];
-	        }
+			cout << setw(3) << answerArray[firstCounter][secondCounter];
+		}
 
 		//積を表示する行数を変える改行
 		cout << '\n';
-	    }
-    }
+	}
+}
