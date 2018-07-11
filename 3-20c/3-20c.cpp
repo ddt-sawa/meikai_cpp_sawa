49948ac3cb5dbd771fe16e4b9dee3cf89000689a
ddt-sawa <ddt.sawa@gmail.com>
2018-07-11 10:39:10 +0900

    mod text, 3-20c インデント適用・前置演算子修正

--- 3-20c/3-20c.cpp
+++ 3-20c/3-20c.cpp
@@ -18,29 +18,29 @@ using namespace std;
 int main()
 {
 	//直角三角形の段数
-    int stepNumber;
+	int stepNumber;
 
-    //段数入力を促す
-    cout << "段数 : ";
+	//段数入力を促す
+	cout << "段数 : ";
 
-    //段数入力
+	//段数入力
 	cin >> stepNumber;
 
 	//n段の直角三角形を表示するループ文
-	for (int firstCounter = 1; firstCounter <= stepNumber; firstCounter++){
+	for (int firstCounter = 1; firstCounter <= stepNumber; ++firstCounter){
 
 		//右下に直角を持ってくるため、段数 - i 個ぶんの空白文字を置くループ文
-		for (int secondCounter = 0; secondCounter < stepNumber - firstCounter; secondCounter++) {
-            //空白文字を表示
+		for (int secondCounter = 0; secondCounter < stepNumber - firstCounter; ++secondCounter) {
+			//空白文字を表示
 			cout << ' ';
 		}
 		//i個の記号を表示するループ文
-		for (int thirdCounter = 0; thirdCounter < firstCounter; thirdCounter++) {
+		for (int thirdCounter = 0; thirdCounter < firstCounter; ++thirdCounter) {
 
-            //記号を表示
+			//記号を表示
 			cout << '*';
 		}
 		//段を変えるため改行
 		cout << '\n';
-    }
+	}
 }
