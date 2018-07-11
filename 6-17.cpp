fd3ac98ce0d443520271019b8d2c99b75129e5eb
ddt-sawa <ddt.sawa@gmail.com>
2018-07-11 18:16:58 +0900

    add text, 6-17

---
+++ 6-17.cpp
@@ -0,0 +1,48 @@
+/*
+ * 6-17.cpp
+ *
+ *  Created on: 2018/06/24
+ *      Author: syuka
+ */
+
+
+/*演習6-17 呼び出された回数を表示する関数put_coutnを作成せよ。
+ * void put_count();
+ */
+#include<iostream>
+
+using namespace std;
+
+//静的記憶域期間を持たせるため、関数が呼び出された回数を関数外で宣言
+int putCountCounter = 0;
+
+//関数名         : putCount
+//仕様           : 自身が呼び出された回数を表示する
+//引数           : なし
+//返り値         : なし
+//作成者         : sawa
+//日付           : 7/11
+void putCount()
+{
+	//自身が呼び出された回数を表示
+	cout << "putCountが呼び出されたのは " << ++putCountCounter << "回目です。\n";
+}
+
+int main()
+{
+
+	//関数を呼び出す回数
+	int integerNumber;
+
+	//関数を呼び出す回数の入力を促す
+	cout << "関数を呼び出す回数 : ";
+
+	//入力
+	cin >> integerNumber;
+
+	//関数を入力された回数ぶん呼び出すループ
+	for(int firstCounter = 0; firstCounter < integerNumber; ++firstCounter) {
+		//関数呼び出し
+		putCount();
+	}
+}
