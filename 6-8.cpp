39460c49a3aa4d4305a1a6f8e3cbb38060b23c14
ddt-sawa <ddt.sawa@gmail.com>
2018-07-17 10:48:02 +0900

    mod text, 6-8 関数ヘッダをjavadocに変更

--- 6-8.cpp
+++ 6-8.cpp
@@ -14,40 +14,41 @@
 using namespace std;
 
 
-//関数名         : putCharacter
-//仕様           : putStarsに送られた現在の段目ぶん表示文字を出力する
-//引数           : int型変数currentNumber(段目)、char型変数stepCharacter(表示文字)
-//返り値         : なし
-//作成者         : sawa
-//日付           : 7/11
+/**
+*putStars関数に送られた現在表示中の段目ぶん表示文字を出力する
+* @param currentStep 現在表示中の段目 stepCharacter 表示文字
+* @author Sawa
+* @since 7.17
+*/
 void putCharacter(int currentStep, char stepCharacter)
 {
 	//現在の段目ぶん表示文字を出力するループ文
 	for (int currentSymbol = 0; currentSymbol < currentStep; ++currentSymbol) {
 
-	//表示文字を出力
+		//表示文字を出力
 		cout << stepCharacter;
 	}
 }
 
 
-//関数名         : putStars
-//仕様           : putCharacter関数を段数ぶん呼び出し、現在の段目と表示文字を渡す
-//引数           : int型変数stepNumber(段数)、char型変数stepCharacter(表示文字)
-//返り値         : なし
-//作成者         : sawa
-//日付           : 7/11
+/**
+*putCharacter関数を段数ぶん呼び出し、現在の段目と表示文字を渡す
+* @param　stepNumber 表示する三角形の段数 stepCharacter 表示文字
+* @author Sawa
+* @since 7.17
+*/
+
 void putStars(int stepNumber, char stepCharacter)
 {
 	//段数ぶんputCharacter関数を呼び出すループ文
 	for (int currentStep = 1; currentStep <= stepNumber; ++currentStep){
 
-	//putCharacter関数を呼び出し、現在表示中の段目と表示文字を渡す
-	putCharacter(currentStep, stepCharacter);
+		//putCharacter関数を呼び出し、現在表示中の段目と表示文字を渡す
+		putCharacter(currentStep, stepCharacter);
 
-	//表示段目を変えるため改行
-	cout << "\n";
-    }
+		//表示段目を変えるため改行
+		cout << "\n";
+	}
 }
 
 
@@ -61,21 +62,21 @@ int main()
 	int stepNumber;
 
 	//段数入力を促す
-    cout << "段数は : ";
+	cout << "段数は : ";
 
-    //段数入力
-    cin >> stepNumber;
+	//段数入力
+	cin >> stepNumber;
 
-    //表示文字の宣言
-    char stepCharacter;
+	//表示文字の宣言
+	char stepCharacter;
 
-    //表示文字の入力を促す
+	//表示文字の入力を促す
 	cout << "表示したい文字は : ";
 
 	//表示文字入力
 	cin >> stepCharacter;
 
-    //putStars関数に段数と表示文字を渡す
+	//putStars関数に段数と表示文字を渡す
 	putStars(stepNumber, stepCharacter);
 }
 
