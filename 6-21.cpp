b5bf235f7937fe6a6ecd6da7a82346b7cc85ecf1
ddt-sawa <ddt.sawa@gmail.com>
2018-07-17 13:34:19 +0900

    mod text, 6-21 関数ヘッダをjavadocに変更・long double型をlong型と誤表記していた問題を修正・多重定義の確認を一種類しかしていなかった問題を修正

--- 6-21.cpp
+++ 6-21.cpp
@@ -20,72 +20,78 @@
 
 using namespace std;
 
-//関数名         : returnAbsoluteValue
-//仕様           : 引数の絶対値を返却する
-//引数           : short型変数argumentValue
-//返り値         : short型変数argumentValueの絶対値
-//作成者         : sawa
-//日付           : 7/11
+/**
+*引数の絶対値を返却する
+* @param argumentValue 引数
+* @return short型の値　引数の絶対値　
+* @author Sawa
+* @since 7.17
+*/
 short returnAbsoluteValue(short argumentValue)
 {
 	//引数の絶対値を返却する
 	return  argumentValue >= 0 ? argumentValue : -argumentValue;
 }
 
-//関数名         : returnAbsoluteValue
-//仕様           : 引数の絶対値を返却する
-//引数           : int型変数argumentValue
-//返り値         : int型変数argumentValueの絶対値
-//作成者         : sawa
-//日付           : 7/11
+/**
+*i引数の絶対値を返却する
+* @param argumentValue 引数
+* @return int型の値　引数の絶対値　
+* @author Sawa
+* @since 7.17
+*/
 int returnAbsoluteValue(int argumentValue)
 {
 	//引数の絶対値を返却する
 	return  argumentValue >= 0 ? argumentValue : -argumentValue;
 }
 
-//関数名         : returnAbsoluteValue
-//仕様           : 引数の絶対値を返却する
-//引数           : long型変数argumentValue
-//返り値         : long型変数argumentValueの絶対値
-//作成者         : sawa
-//日付           : 7/11
+/**
+*引数の絶対値を返却する
+* @param argumentValue 引数
+* @return long型の値　引数の絶対値　
+* @author Sawa
+* @since 7.17
+*/
 long returnAbsoluteValue(long argumentValue)
 {
 	//引数の絶対値を返却する
 	return  argumentValue >= 0 ? argumentValue : -argumentValue;
 }
 
-//関数名         : returnAbsoluteValue
-//仕様           : 引数の絶対値を返却する
-//引数           : float型変数argumentValue
-//返り値         : float型変数argumentValueの絶対値
-//作成者         : sawa
-//日付           : 7/11
+/**
+*引数の絶対値を返却する
+* @param argumentValue 引数
+* @return float型の値　引数の絶対値　
+* @author Sawa
+* @since 7.17
+*/
 float returnAbsoluteValue(float argumentValue)
 {
 	//引数の絶対値を返却する
 	return  argumentValue >= 0 ? argumentValue : -argumentValue;
 }
 
-//関数名         : returnAbsoluteValue
-//仕様           : 引数の絶対値を返却する
-//引数           : double型変数argumentValue
-//返り値         : double型変数argumentValueの絶対値
-//作成者         : sawa
-//日付           : 7/11
+/**
+*引数の絶対値を返却する
+* @param argumentValue 引数
+* @return double型の値　引数の絶対値　
+* @author Sawa
+* @since 7.17
+*/
 double returnAbsoluteValue(double argumentValue)
 {
 	//引数の絶対値を返却する
 	return  argumentValue >= 0 ? argumentValue : -argumentValue;
 }
 
-//関数名         : returnAbsoluteValue
-//仕様           : 引数の絶対値を返却する
-//引数           : longdouble型変数argumentValue
-//返り値         : longdouble型変数argumentValueの絶対値
-//作成者         : sawa
-//日付           : 7/11
+/**
+*引数の絶対値を返却する
+* @param argumentValue 引数
+* @return long double型の値　引数の絶対値　
+* @author Sawa
+* @since 7.17
+*/
 long double returnAbsoluteValue(long double argumentValue)
 {
 	//引数の絶対値を返却する
@@ -93,15 +99,41 @@ long double returnAbsoluteValue(long double argumentValue)
 }
 int main()
 {
-	//最後に多重定義したlong double型の変数宣言
-	long double argumentValue;
+	//short型変数
+	short shortArgumentValue = 5;
 
-	//値の入力を促す
-	cout << "絶対値を調べたい値 : ";
+	//int型変数
+	int intArgumentValue = 10;
 
-	//入力
-	cin >> argumentValue;
+	//long型変数
+	long longArgumentValue = 20;
 
-	//関数に値を渡し、返却された絶対値を表示する
-	cout << returnAbsoluteValue(argumentValue) << "です。\n";
+	//float型変数
+	float floatArgumentValue = 0.4;
+
+	//double型変数
+	double doubleArgumentValue = 0.1;
+
+	//long double型変数
+	long double longDoubleArgumentValue = 0.01;
+
+
+	//short型変数の表示
+	cout << returnAbsoluteValue(shortArgumentValue) << "です。\n";
+
+	//int型変数の表示
+	cout << returnAbsoluteValue(intArgumentValue) << "です。\n";
+
+	//long型変数の表示
+	cout << returnAbsoluteValue(longArgumentValue) << "です。\n";
+
+	//float型変数の表示
+	cout << returnAbsoluteValue(floatArgumentValue) << "です。\n";
+
+	//double型変数の表示
+	cout << returnAbsoluteValue(doubleArgumentValue) << "です。\n";
+
+	//long double型変数の表示
+	cout << returnAbsoluteValue(longDoubleArgumentValue) << "です。\n";
 }
+
