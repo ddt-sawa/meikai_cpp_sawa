5ca84444ba67ce23c181d1a6a3baedd5e6b0268b
ddt-sawa <ddt.sawa@gmail.com>
2018-07-26 20:44:10 +0900

    mod text, 6-12 引数名・インデント・コメント修正  引数省略時の動作確認追加

--- 6-12.cpp
+++ 6-12.cpp
@@ -5,9 +5,9 @@
  *      Author: syuka
  */
 
-/*演習6-12 b以上a以下の全整数の和を求める関数sumを作成せよ。なお、
- * bに対する実引数が省略されて呼び出された場合は、bを1とみなして合計を求めること。
- */
+ /*演習6-12 b以上a以下の全整数の和を求める関数sumを作成せよ。なお、
+  * bに対する実引数が省略されて呼び出された場合は、bを1とみなして合計を求めること。
+  */
 
 #include<iostream>
 
@@ -15,26 +15,26 @@ using namespace std;
 
 
 /**
-*第二引数以上第一引数以下の全整数の総和を返却する、第二引数の値が指定されなかった場合、第二引数は1とする
-* @param firstArgument 第一引数、secondArgument 第二引数
+* 下限値以上、上限値以下の全整数の総和を返却する。下限値が指定されなかった場合、下限値は1とする
+* @param upperLimit 上限値、lowerLimit 下限値
 * @return entireSum 総和
 * @author Sawa
 * @since 7.17
 */
-int returnSum(int firstArgument,int secondArgument = 1)
+int returnSum(int upperLimit, int lowerLimit = 1)
 {
 	//第二引数以上第一引数以下の全整数の総和を0で初期化
-    int entireSum = 0;
+	int entireSum = 0;
 
-    //総和を計算するループ
-    for (int counterVariable = secondArgument; counterVariable <= firstArgument;  ++counterVariable) {
+	//総和を計算するループ
+	for (int counterVariable = lowerLimit; counterVariable <= upperLimit; ++counterVariable) {
 
-    	//総和に整数値を代入
-    	entireSum += counterVariable;
-    }
+		//総和に整数値を代入
+		entireSum += counterVariable;
+	}
 
-//総和を返却
-    return entireSum;
+	//総和を返却
+	return entireSum;
 }
 
 int main()
@@ -42,24 +42,27 @@ int main()
 	//表示する値の概要を告知
 	cout << "b以上a以下の全整数の総和を求めます。\n";
 
-	//整数a
-	int firstArgument;
+	//上限値
+	int upperLimit;
 
-	//aの入力を促す
-    cout << "aの値 : ";
+	//上限値の入力を促す
+	cout << "aの値 : ";
 
-    //aの入力
-    cin >> firstArgument;
+	//上限値の入力
+	cin >> upperLimit;
 
-    //整数b
-    int secondArgument;
+	//下限値
+	int lowerLimit;
 
-    //bの入力を促す
-    cout << "bの値 : ";
+	//下限値の入力を促す
+	cout << "bの値 : ";
 
-    //bの入力
-    cin  >> secondArgument;
+	//下限値の入力
+	cin >> lowerLimit;
 
-    //sum関数へ引数を渡し、返却値となる総和を表示
-    cout << "合計値は" << returnSum(firstArgument, secondArgument) << "です。\n";
+	//関数へ引数を渡し、返却値となる総和を表示
+	cout << "合計値は" << returnSum(upperLimit, lowerLimit) << "です。\n";
+
+	//引数が1つの場合でも下限値==1として動作することを確認
+	cout << "合計値は" << returnSum(upperLimit) << "です。\n";
 }
