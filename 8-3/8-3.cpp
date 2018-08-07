aba1b85466978672eac99724f30dd112743dfcd3
ddt-sawa <ddt.sawa@gmail.com>
2018-08-07 11:03:00 +0900

    mod text, 8-3 配列の要素数を定数に修正・後置演算子を前置に修正・Javadocのコメントを修正

--- 8-3/8-3.cpp
+++ 8-3/8-3.cpp
@@ -5,9 +5,9 @@
  *      Author: ddt
  */
 
-/*演習8-3 文字列中の大文字を小文字に変換した上で表示する関数put_lowerを作成せよ。
- * void put_ lower(const char s[]);
- */
+ /*演習8-3 文字列中の大文字を小文字に変換した上で表示する関数put_lowerを作成せよ。
+  * void put_ lower(const char s[]);
+  */
 
 //tolower関数を使うため、<cctype>ヘッダをインクルード
 #include<cctype>
@@ -16,7 +16,7 @@
 using namespace std;
 
 /**
-* 文字列中の大文字を子文字に変換する
+* 文字列中の大文字を小文字に変換する
 * @param characterArray 文字列
 * @author Sawa
 * @since 7.20
@@ -24,7 +24,8 @@ using namespace std;
 void putLower(const char characterArray[])
 {
 	//受け取った文字列中の大文字を小文字に変換するループ
-	for (int firstCounter = 0; characterArray[firstCounter]; firstCounter++) {
+	for (int firstCounter = 0; characterArray[firstCounter]; ++firstCounter) {
+
 		//要素の大文字を小文字に変換
 		cout << static_cast<char>(tolower(characterArray[firstCounter]));
 	}
@@ -32,8 +33,11 @@ void putLower(const char characterArray[])
 
 int main()
 {
+	//配列の要素数
+	const int arraySize = 36;
+
 	//文字列配列を初期化
-	char characterArray[36] = { 0 };
+	char characterArray[arraySize] = { 0 };
 
 	//大文字を含んだ文字列の入力を促す
 	cout << "文字列中の大文字を小文字にして表示します。 　文字列 : ";
