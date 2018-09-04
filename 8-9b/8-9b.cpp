7a2cfe73a056e9a117ad216ec3b73b15993e1a9f
ddt-sawa <ddt.sawa@gmail.com>
2018-09-04 18:04:13 +0900

    mod text, 8-9b 最初に配列サイズを固定していた問題を修正

--- 8-9b/8-9b.cpp
+++ 8-9b/8-9b.cpp
@@ -1,22 +1,10 @@
-/*
- * 8-9.cpp
- *
- *  Created on: 2018/06/26
- *      Author: ddt
- */
-
  /*演習8-9 p308で学習したstrcpy関数及びstrncpy関数と同等な関数を作成せよ。
  * b.strncpy関数の作成
  */
 
- //strlen関数を使うため、<cstring>ヘッダをインクルード
-#include<cstring>
 #include<iostream>
 
 using namespace std;
-
-//配列の容量を多めに宣言
-const int arrayCapacity = 30;
 
 /**
 * 第二引数の文字列を、第一引数に指定した文字数までコピーする。コピー元の文字列が指定文字数より短い場合、残りをナル文字で埋め尽くす
@@ -25,11 +13,13 @@ const int arrayCapacity = 30;
 * @author Sawa
 * @since 7.23
 */
-
-char* returnCopyArray(char* firstString, const char* secondString, int limitNumber)
+char* copyString(char* firstString, const char* secondString, size_t limitNumber)
 {
-	//コピー元文字列の長さを測る
-	int secondStringLength = strlen(secondString);
+	//コピー元文字列の長さ
+	int secondStringLength = 0;
+
+	//コピー元文字列の長さを取得
+	for (; secondString[secondStringLength]; ++secondStringLength);
 
 	//コピー元文字列の長さが指定文字数以上の場合
 	if (secondStringLength >= limitNumber) {
@@ -50,7 +40,6 @@ char* returnCopyArray(char* firstString, const char* secondString, int limitNumb
 			//コピー先の要素にコピー元の要素を代入
 			firstString[firstCounter] = secondString[firstCounter];
 		}
-
 		//指定文字に達するまで末尾にナル文字を埋め込むループ
 		for (int firstCounter = 1; firstCounter <= limitNumber - secondStringLength; ++firstCounter) {
 
@@ -64,36 +53,22 @@ char* returnCopyArray(char* firstString, const char* secondString, int limitNumb
 
 int main()
 {
-	//コピー先文字列を初期化
-	char firstString[arrayCapacity] = { 0 };
+	//文字列1
+	char firstString[] = "ABCD";
 
-	//文字列入力を促す
-	cout << "文字列1を入力してください : ";
+	//コピー元の文字列配列を初期化
+	char secondString[] = "EFG";
 
-	//文字列を入力
-	cin >> firstString;
-
-	//コピー元文字列を初期化
-	char secondString[arrayCapacity] = { 0 };
-
-	//文字列入力を促す
-	cout << "文字列2を入力してください : ";
-
-	//文字列を入力
-	cin >> secondString;
-
-	//コピーする文字数を初期化
+	//指定文字数
 	int limitNumber = 0;
 
-	//文字数入力を促す
-	cout << "コピーする最大の文字数を入力してください。\n";
-
-	//文字数入力
+	//指定文字数入力
 	cin >> limitNumber;
 
-	//コピーを行った文字列を表示
-	cout << "コピーした文字列は " << returnCopyArray(firstString, secondString, limitNumber) << " です。\n";
+	//コピーした文字列を表示
+	cout << "コピーした文字列は " << copyString(firstString, secondString, limitNumber) << " です。\n";
 }
+
 
 
 
