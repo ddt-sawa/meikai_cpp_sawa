171580ffdd4a674db5919bfff64c3a17986667b7
ddt-sawa <ddt.sawa@gmail.com>
2018-09-05 17:54:44 +0900

    mod text, 8-14 インデント修正

--- 8-14/8-14.cpp
+++ 8-14/8-14.cpp
@@ -1,13 +1,14 @@
-/*
+﻿/*
  * 8-14.cpp
  *
  *  Created on: 2018/06/26
  *      Author: ddt
  */
 
-/*演習8-14 文字列sの文字の並びを反転する関数str_rvsを作成せよ。
- * char* str_rvs(char* s)
- */
+ /*演習8-14 文字列sの文字の並びを反転する関数str_rvsを作成せよ。
+  * char* str_rvs(char* s)
+  * 例えば、文字列sが"abc"であれば、その文字列を"cba"に更新する。受け取ったsの値をそのまま返却すること。
+  */
 
 //strlen関数を使うため、cstringをインクルード
 #include<cstring>
@@ -29,16 +30,16 @@ char* reverseArray(char* characterArray)
 	int characterArrayLength = strlen(characterArray);
 
 	//文字列の並びを入れ替えるループ
-	for (int firstCounter = 0; firstCounter < (characterArrayLength / 2); ++firstCounter){
+	for (int firstCounter = 0; firstCounter < (characterArrayLength / 2); ++firstCounter) {
 
 		//走査中の値を一時保存
 		int temporaryValue = characterArray[firstCounter];
 
 		//走査中の要素と対称に並んでいる要素の値を、走査中の値に代入
-        characterArray[firstCounter] = characterArray[characterArrayLength - firstCounter - 1];
+		characterArray[firstCounter] = characterArray[characterArrayLength - firstCounter - 1];
 
 		//一時保存していた走査中の値を対称に並んでいる要素に代入
-        characterArray[characterArrayLength - firstCounter - 1] = temporaryValue;
+		characterArray[characterArrayLength - firstCounter - 1] = temporaryValue;
 
 	}
 	//入れ替えた文字列を返却する
@@ -48,18 +49,18 @@ char* reverseArray(char* characterArray)
 int main()
 {
 	//反転する文字列配列の要素数
-		const int arrayCapacity = 50;
+	const int arrayCapacity = 50;
 
-		//文字列配列を初期化
-		char characterArray[arrayCapacity] = {0};
+	//文字列配列を初期化
+	char characterArray[arrayCapacity] = { 0 };
 
-		//文字列配列の入力を促す
-		cout << "反転する文字列を入力してください。\n";
-		
-		//入力
-		cin >> characterArray;
+	//文字列配列の入力を促す
+	cout << "反転する文字列を入力してください。\n";
 
-		//反転した文字列を表示
-        cout << "反転した文字列は" << reverseArray(characterArray) << "です。\n";
+	//入力
+	cin >> characterArray;
+
+	//反転した文字列を表示
+	cout << "反転した文字列は" << reverseArray(characterArray) << "です。\n";
 
 }
